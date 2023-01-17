8234ead3841a92386bb7f624ca6596b06536ee10
Rollo M <rollo.moore@nhs.net>
2023-01-17 12:39:12 +0000

    update of JASP file and the picture PNG files it created

--- Rreview/snip_plot-time-series.R
+++ Rreview/snip_plot-time-series.R
@@ -5,3 +5,5 @@ prepAllG %>% ggplot(aes(Gy,Date)) +
 filter(prepAllG, Energy==70) %>% ggplot(aes(Gy,Date)) +
   +   geom_line(col = palette_light()[1]) +
   +   geom_point(col = palette_light()[1])
+
+filter(prepAllG, Energy=="70") %>% group_by(ChamberSN) %>% plot_time_series(Date,Gy, .facet_ncol=2, .interactive=FALSE)
